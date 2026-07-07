125c788681e93c5f652bcf759995dbbebcc82dd4
GauravBanik <gauravbanikorg@gmail.com>
2026-07-07 08:35:39 +0530

    lest see

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from GitHub Actions!"
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test_app.py
@@ -0,0 +1,12 @@
+from app import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -5
+
+
+def test_add_zero():
+    assert add(10, 0) == 10
